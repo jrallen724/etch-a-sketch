f8b56929fafda648088428e049bcde9751423b23
jrallen724 <jrallen724@gmail.com>
2023-04-11 12:58:38 -0400

    Completed main objectives of Etch-A-Sketch project

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const container = document.querySelector('#container');
+const resetBtn = document.querySelector('#reset');
+let divDimension = 0;
 
 
-function createGrid (size) {    
-    for (let i = 0; i < size; i++) {
-        let div = document.createElement('div');
-        container.getElementsByClassName.gridTemplateColumns = 
+function createGrid (size) {
+    divDimension = 600 / size - 2;    
+    for (let i = 0; i < (size * size); i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.setAttribute('style', 'height: ' + divDimension + 'px; width: ' + divDimension + 'px');
+        div.addEventListener('mouseover', function () {
+            div.style.backgroundColor = 'black';
+        })
         container.appendChild(div);
     }
 }
+
+function removeGrid (parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+resetBtn.addEventListener('click', function () {
+    divDimension = parseInt(prompt('Select a size: '));
+    if (divDimension > 100) {
+        divDimension = 100;
+    }
+    removeGrid(container);
+    createGrid(divDimension);
+})
 
 createGrid(16);
